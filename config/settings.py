46569c206cdf78c22e06308d0aa3e0821e2862a1
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-09 00:44:16 +0900

    adminで更新確認

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'library.apps.LibraryConfig'
+    'rest_framework',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
