b08a209373f42b4f95f9432060938b81e148869d
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-08 23:35:54 +0900

    アプリ再作成

--- library/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-class BookSerialiser(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        verbose_name = "book"
--- library/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import BookAPIView
-from django.urls import path
-
-urlpatterns = [
-    path('books/', BookAPIView.as_view())
-]
