990f7b80344d2c0c2818998ae52d09282f9fc819
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-09 23:58:46 +0900

    特定IDのデータの取得確認

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,5 @@ from .views import LibraryCreateAPIView, LibraryListAPIView, LibraryRetriveAPIVi
 urlpatterns = [
     path('libraries/create', LibraryCreateAPIView.as_view()),
     path('libraries/', LibraryListAPIView.as_view()),
-    path('libraries/<pk:int>', LibraryRetriveAPIView.as_view())
+    path('libraries/<pk>', LibraryRetriveAPIView.as_view())
 ]
