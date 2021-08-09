af3bf5c755813f91e2b332d3a26afc1fb2efafc9
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-10 00:00:54 +0900

    削除パス追加

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LibraryCreateAPIView, LibraryListAPIView, LibraryRetriveAPIView
+from .views import LibraryCreateAPIView, LibraryDestroyAPIView, LibraryListAPIView, LibraryRetriveAPIView
 
 urlpatterns = [
     path('libraries/create', LibraryCreateAPIView.as_view()),
     path('libraries/', LibraryListAPIView.as_view()),
-    path('libraries/<pk>', LibraryRetriveAPIView.as_view())
+    path('libraries/<pk>', LibraryRetriveAPIView.as_view()),
+    path('libraries/<pk>', LibraryDestroyAPIView.as_view()),
 ]
