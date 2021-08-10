e05a147164a402f3dfa0f394448658709659d80a
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-10 14:53:21 +0900

    swaggerUI起動確認

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger API
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Snippets API',
+        default_version='v1'
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('library.urls'))
+    path('api/', include('library.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
