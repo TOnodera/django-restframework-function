91a49c23883395efa0a47df5c026c8ad402735ce
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-10 16:34:53 +0900

    bookモデル作成

--- library/models.py
+++ library/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-# Create your models here.
-
 class Library(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    Library = models.ForeignKey(Library, on_delete=models.CASCADE)
